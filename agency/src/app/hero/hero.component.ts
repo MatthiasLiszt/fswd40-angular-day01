26e9c353c224db3ed33529aa1ec8602ff57c88e6
Patrick Klostermann <dev@netflair.at>
2018-05-23 12:10:35 +0200

    added hero section

--- agency/src/app/hero/hero.component.ts
+++ agency/src/app/hero/hero.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+	private heroHeading: string;
+	private heroText: string;
+	private heroBtnText: string;
+	private heroBtnUrl: string;
 
-  constructor() { }
+  constructor() {
+  	this.heroHeading = "We are your ultimate Winter experience Provider";
+  	this.heroText = "With hundreds of winter holiday destinations offered, we are your nnatural choise for planning it all out";
+	this.heroBtnText= "Learn more";
+	this.heroBtnUrl = "#";  
+  }
 
   ngOnInit() {
   }
