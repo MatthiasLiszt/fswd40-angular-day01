34129e2b92cff106a929ddd5fde162862a544bc8
Patrick Klostermann <dev@netflair.at>
2018-05-23 15:37:55 +0200

    replaced text in .html to variables and defined them in .ts

--- agency/src/app/home/home.component.ts
+++ agency/src/app/home/home.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	private homeHeading: string;
+	private homeCol1H: string;
+	private homeCol2H: string;
+	private homeCol3H: string;
+	private homeCol4H: string;
 
-  constructor() { }
+  constructor() {
+  	this.homeHeading = "Why we are the best";
+  	this.homeCol1H = "Accommodation";
+  	this.homeCol2H = "Transportation";
+  	this.homeCol3H = "Seasoned Agents";
+  	this.homeCol4H = "Easy Trip Planning";
+  }
 
   ngOnInit() {
   }
