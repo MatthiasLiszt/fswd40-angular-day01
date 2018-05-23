0ee71c02de484fe2b3e084d225a184b8cda4327c
Patrick Klostermann <dev@netflair.at>
2018-05-23 13:58:43 +0200

    corrected spelling

--- agency/src/app/hero/hero.component.ts
+++ agency/src/app/hero/hero.component.ts
@@ -13,7 +13,7 @@ export class HeroComponent implements OnInit {
 
   constructor() {
   	this.heroHeading = "We are your ultimate Winter experience Provider";
-  	this.heroText = "With hundreds of winter holiday destinations offered, we are your nnatural choise for planning it all out";
+  	this.heroText = "With hundreds of winter holiday destinations offered, we are your natural choice for planning it all out";
 	this.heroBtnText= "Learn more";
 	this.heroBtnUrl = "#";  
   }
