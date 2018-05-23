2787825b04205906139874d43d1239293f7e851f
Patrick Klostermann <dev@netflair.at>
2018-05-23 10:36:02 +0200

    added hero and nav-bar components

--- agency/src/app/app.module.ts
+++ agency/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavBarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
